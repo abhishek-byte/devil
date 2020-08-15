017ae1fada87b822e91ac1f98dc0b8f3c6d1fe22
abhishek-byte <github.com/abhishek-byte>
2020-08-15 08:17:45 -0700

    Updates

---
+++ precache-manifest.5842e7973dc2f381ef925ac5777f252e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d53e6f40534f4171123f55456fdca205",
+    "url": "/devil/index.html"
+  },
+  {
+    "revision": "6bbe66fd916d9d460663",
+    "url": "/devil/static/css/main.9182ae0b.chunk.css"
+  },
+  {
+    "revision": "c20569018e7ec5331715",
+    "url": "/devil/static/js/2.09e051c2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/devil/static/js/2.09e051c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6bbe66fd916d9d460663",
+    "url": "/devil/static/js/main.ca2d6a95.chunk.js"
+  },
+  {
+    "revision": "c4436790e7ff291dabdc",
+    "url": "/devil/static/js/runtime-main.8af9c626.js"
+  },
+  {
+    "revision": "9a76e74e107e6163dc82116b8236c132",
+    "url": "/devil/static/media/login.9a76e74e.svg"
+  }
+]);
